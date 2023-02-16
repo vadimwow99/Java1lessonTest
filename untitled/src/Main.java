bc985ed208088d00a2d68266c9cb519e6ddb99b1
vagin <vagin999@gmail.com>
2023-02-16 10:48:14 +0200

    hw is done

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static void task1(){
+        System.out.println("Homework done!");
+    }
 }
